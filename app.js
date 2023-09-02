8fa41148e9012faf464eb9b93a8a34260a1e2bed
Dvorak <mojiwab@proton.me>
2023-09-02 19:35:35 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+
+const dbString = `mongodb+srv://courageTheCowardlyDog:e2VkXTwpdQzDa19v@dummyprojects.hykmfdu.mongodb.net/usePopcorn`;
+const port = 5050;
+
+mongoose.connect(dbString).then((con) => {
+    console.log('Connection done at - ', dbString)
+});
+
+app.use('/test', (req, res, next) => {
+    res.json('Hello and welcome.').status(200);
+});
+
+app.listen(port, () => {
+    console.log('App started');
+})
