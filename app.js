adad6ec328ed39ffd20e291c3c74f03ec0943195
Dvorak <mojiwab@proton.me>
2023-09-02 19:45:02 +0530

    Add root route

--- app.js
+++ app.js
@@ -10,6 +10,10 @@ mongoose.connect(dbString).then((con) => {
     console.log('Connection done at - ', dbString)
 });
 
+app.use('/', (req, res, next) => {
+    res.json('Hello and welcome.').status(200);
+});
+
 app.use('/test', (req, res, next) => {
     res.json('Hello and welcome.').status(200);
 });
